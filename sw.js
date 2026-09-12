6f003daf859ddf1a9b22448371fc0029d58b056b
Riva-Fencer <thevaibhavraut@gmail.com>
2026-09-12 19:46:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,60 @@
-const CACHE_NAME = 'riva-fence-v1';
+/**
+ * Riva Fencer - Service Worker (Try #8 + Tab 3 Offline Models)
+ * Caches shell, cartridges, and MediaPipe offline assets.
+ */
 
-// Install: Activate new worker immediately
+const CACHE_NAME = 'riva-fencer-v2';
+
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-512.png',
+    './sparrer.js',
+    './clinic.js',
+    './drills/drill_001.js',
+    './drills/drill_002.js',
+    './drills/drill_003.js',
+    './models/pose_landmarker_lite.task',
+    './models/vision_wasm_internal.js',
+    './models/vision_wasm_internal.wasm'
+];
+
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        }).then(() => self.skipWaiting())
+    );
 });
 
-// Activate: Clear out old cache so new edits take effect
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
-// Fetch: Always try getting the newest version from the internet first
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request).then((networkResponse) => {
+                return networkResponse;
+            });
+        }).catch(() => {
+            if (event.request.mode === 'navigate') {
+                return caches.match('./index.html');
+            }
+        })
+    );
 });
